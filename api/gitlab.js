8236e94ed52ebb3b02de3a897a14b75ccb8d275a
Ryan Cao <47623453+ryanccn@users.noreply.github.com>
2020-05-10 17:40:44 +0800

    Create api/gitlab.js

---
+++ api/gitlab.js
@@ -0,0 +1,31 @@
+module.exports = (request, response) => {
+  const { url } = request
+  const urlParts = url.split('/')
+
+  if (url === '/') {
+    response.writeHead(302, {
+      Location: `https://zeit.co`
+    })
+
+    response.end()
+    return
+  }
+
+  if (urlParts.length < 3) {
+    return {
+      error: 'Please specify all the missing data (see the repo)!',
+      errorHandle: 'missing_data'
+    }
+  }
+
+  const isZeit = urlParts.length === 3
+  const user = isZeit ? 'zeit' : urlParts[1]
+  const repo = urlParts[isZeit ? 1 : 2]
+  const code = urlParts[isZeit ? 2 : 3]
+
+  response.writeHead(302, {
+    Location: `https://gitlab.com/${user}/${repo}/blob/master/errors/${code}.md`
+  })
+
+  response.end()
+}
