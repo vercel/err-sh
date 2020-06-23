5ffa21df9b4ec9553aa9033b2d142e3b3fe1f73b
Ty Mick <ty@tymick.me>
2020-06-23 17:02:33 -0400

    Rename ZEIT to Vercel (#12)

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ module.exports = (request, response) => {
 
   if (url === '/') {
     response.writeHead(302, {
-      Location: `https://zeit.co`
+      Location: `https://vercel.com`
     })
 
     response.end()
@@ -18,10 +18,10 @@ module.exports = (request, response) => {
     }
   }
 
-  const isZeit = urlParts.length === 3
-  const user = isZeit ? 'zeit' : urlParts[1]
-  const repo = urlParts[isZeit ? 1 : 2]
-  const code = urlParts[isZeit ? 2 : 3]
+  const isVercel = urlParts.length === 3
+  const user = isVercel ? 'vercel' : urlParts[1]
+  const repo = urlParts[isVercel ? 1 : 2]
+  const code = urlParts[isVercel ? 2 : 3]
 
   response.writeHead(302, {
     Location: `https://github.com/${user}/${repo}/blob/master/errors/${code}.md`
