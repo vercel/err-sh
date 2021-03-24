11980ca53b9bd1a082d60e2f7a37f27f2a7083b2
Tim Neutkens <timneutkens@me.com>
2021-03-24 14:39:03 +0100

    Add handling of new err.sh location for Next.js

--- api/index.js
+++ api/index.js
@@ -1,31 +1,37 @@
 module.exports = (request, response) => {
-  const { url } = request
-  const urlParts = url.split('/')
+  const { url } = request;
+  const urlParts = url.split("/");
 
-  if (url === '/') {
+  if (url === "/") {
     response.writeHead(302, {
-      Location: `https://vercel.com`
-    })
+      Location: `https://vercel.com`,
+    });
 
-    response.end()
-    return
+    response.end();
+    return;
   }
 
   if (urlParts.length < 3) {
     return {
-      error: 'Please specify all the missing data (see the repo)!',
-      errorHandle: 'missing_data'
-    }
+      error: "Please specify all the missing data (see the repo)!",
+      errorHandle: "missing_data",
+    };
   }
 
-  const isVercel = urlParts.length === 3
-  const user = isVercel ? 'vercel' : urlParts[1]
-  const repo = urlParts[isVercel ? 1 : 2]
-  const code = urlParts[isVercel ? 2 : 3]
+  const isVercel = urlParts.length === 3;
+  const user = isVercel ? "vercel" : urlParts[1];
+  const repo = urlParts[isVercel ? 1 : 2];
+  const code = urlParts[isVercel ? 2 : 3];
 
-  response.writeHead(302, {
-    Location: `https://github.com/${user}/${repo}/blob/master/errors/${code}.md`
-  })
+  if (user === "vercel" && repo === "next.js") {
+    response.writeHead(302, {
+      Location: `https://nextjs.org/docs/messages/${code}`,
+    });
+  } else {
+    response.writeHead(302, {
+      Location: `https://github.com/${user}/${repo}/blob/master/errors/${code}.md`,
+    });
+  }
 
-  response.end()
-}
+  response.end();
+};
